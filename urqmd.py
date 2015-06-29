8176dc1e83eb59c3fb6e888ef1363a1d83bbe387
jdbrice <jdb12@rice.edu>
2015-06-29 13:14:49 -0500

    convert args to int where needed

--- urqmd.py
+++ urqmd.py
@@ -8,12 +8,12 @@ eventsPerJob = 100
 
 if len( sys.argv ) >= 4 :
 	name = sys.argv[ 1 ]
-	energy = sys.argv[ 2 ]
-	totalEvents = sys.argv[ 3 ]
+	energy = int(sys.argv[ 2 ])
+	totalEvents = int(sys.argv[ 3 ])
 else :
 	exit()
 if len( sys.argv ) == 5 :
-	eventsPerJob = sys.argv[ 4 ]
+	eventsPerJob = int(sys.argv[ 4 ])
 
 
 
