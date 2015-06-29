7a8ead90ccb3dd17bf13e0ccdf0e4a69e7397018
jdbrice <jdb12@rice.edu>
2015-06-29 13:11:38 -0500

    fixed typo

--- urqmd.py
+++ urqmd.py
@@ -10,7 +10,7 @@ if len( sys.argv ) == 4 :
 	name = sys.argv[ 1 ]
 	energy = sys.argv[ 2 ]
 	totalEvents = sys.argv[ 3 ]
-else 
+else :
 	exit()
 if len( sys.argv == 5) :
 	eventsPerJob = sys.argv[ 4 ]
