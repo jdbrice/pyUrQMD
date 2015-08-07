02b6a02eddb0ac7248c0cb472e310ff034a149ae
jdbrice <jdb12@rice.edu>
2015-08-07 12:00:50 -0500

    cleaned up pyurqmd.py

--- pyurqmd.py
+++ pyurqmd.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 import pyUrQMD.project as urqmd
 import sys
+import argparse
 
-print "Usage:\n", "pyurqmd project_name comEnergy totalEvents eventsPerJob"
+parser = argparse.ArgumentParser( description="Creates a slew of input files for submitting UrQMD to a cluster" );
+parser.add_argument( "proj_name", help="project name - just creates a folder with this name and puts input files there" )
+parser.add_argument( "ecm", help="Center of Mass energy" )
+parser.add_argument( "total", help="Total # of events to generate" )
+parser.add_argument( "epj", help="# of events per job" )
 
-eventsPerJob = 100
+args = parser.parse_args()
 
-if len( sys.argv ) >= 4 :
-	name = sys.argv[ 1 ]
-	energy = float(sys.argv[ 2 ])
-	totalEvents = int(sys.argv[ 3 ])
-else :
-	exit()
-if len( sys.argv ) == 5 :
-	eventsPerJob = int(sys.argv[ 4 ])
-
+name = args.proj_name
+energy = float(args.ecm)
+totalEvents = int(args.total)
+eventsPerJob = int(args.epj)
 
 urqmd.createProject( name, energy, totalEvents, eventsPerJob )
